f89d88ba0d2d879bd3e30cf13c3f07c9140b549e
AhmedAboRaya <ahmedaboraya399@gmail.com>
2024-07-22 19:07:23 +0300

    Create empty modal for add new product

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
 import Card from './Components/Card'
+import Modal from './Components/ui/Modal/Modal'
+import Button from './Components/ui/Buttons/Button'
+import { useState } from 'react'
 
 
 function App() {
 
+  /*_________ STATE _______*/
+  let [isOpen, setIsOpen] = useState(false)
+
+  /*_________ HANDLER _______*/
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
+  /*_________ RENDER _______*/
+
   return (
-    <div className='container mx-auto'>
-      <Card />
-    </div>
+    <>
+    
+      <div className='container mx-auto pt-4'>
+        <Button bg='bg-blue-700' value='Add Product' width='w-full' onClickFN={openModal}/>
+        <Card /> 
+      </div>
+
+      <Modal closeModal={closeModal} isOpen={isOpen} title='Add New Product'> 
+
+
+        <div className='flex items-center space-x-2'>
+          <Button bg='bg-blue-600' value='Submit' width='w-full'/>
+          <Button bg='bg-gray-500' value='Cancel' width='w-full' onClick={closeModal}/>
+        </div>
+
+      </Modal>
+    </>
   )
 }
 
--- src/Components/ui/Buttons/Button.tsx
+++ src/Components/ui/Buttons/Button.tsx
@@ -4,11 +4,12 @@ interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     value: string;
     bg: string;
     width?: "w-full" | "w-fit";
+    onClickFN?: ()=>void;
 }
 
-const Button: React.FC<IProps> = ({ value, bg, width, ...rest }) => {
+const Button: React.FC<IProps> = ({ value, bg, width, onClickFN, ...rest }) => {
     return (
-        <button 
+        <button  onClick={onClickFN}
             {...rest} 
             className={`${bg} ${width}  mb-2 text-lg rounded-md text-white py-1`}>
             {value}
